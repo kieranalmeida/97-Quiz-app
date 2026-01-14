cd5ce4f4cb9b0afce580541b721963bb1cc8183e
Kieran <kieran1000@icloud.com>
2026-01-14 12:46:20 +0000

    Start page

--- App.jsx
+++ App.jsx
@@ -1,9 +1,24 @@
 import React from "react"
 
 export default function App() {
+    // Controls the state of the quiz (whether or not it has started)
+    const [quizStarted, setQuizStarted] = React.useState(false)
+
+    // Starts the quiz
+    function startQuiz() {
+        setQuizStarted(true)
+    }
+
     return (
         <>
-        
+        {!quizStarted &&
+            <main className="start-page">
+                <h1 className="start-page-title">Quizzical</h1>
+                <h2 className="start-page-desc">Answer 5 random trivia questions</h2>
+                <button className="start-btn" onClick={startQuiz}>Start quiz</button>
+                <img className="blob-top" src="images/blob-top.png"></img>
+                <img className="blob-bottom" src="images/blob-bottom.png"></img>
+            </main>}
         </>
     )
 }
